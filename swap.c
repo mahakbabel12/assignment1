9add131fb4b2e6a4f25c27d783b4b15f3c6616ea
mahakbabel12 <140581938+mahakbabel12@users.noreply.github.com>
2023-07-28 19:39:37 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a=2,b=4,temp;
+    printf("\nbefore Swapping: a = %d, b = %d", a, b);
+    temp=a;
+    a=b;
+    b=temp;
+    printf("\nAfter Swapping: a = %d, b = %d", a, b);
+
+
+}
